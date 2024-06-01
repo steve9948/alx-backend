5691f324ef7507a6c0ca3fdf30142f83a9691710
steve9948 <mbuguagichimu@gmail.com>
2024-06-01 15:02:52 +0000

    Removed a silly mistake

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,15 +7,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', strict_slashes =flase)
+def index() -> str:
     """
     Route for the index page.
 
     Returns:
         The rendered template for the index page.
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
