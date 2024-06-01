c0fad86659dfad252c5ec57fabdadd506e3a76ce
steve9948 <mbuguagichimu@gmail.com>
2024-06-01 15:11:25 +0000

    Stict error

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes =flase)
+@app.route('/', strict_slashes =False)
 def index() -> str:
     """
     Route for the index page.
